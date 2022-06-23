75417472c1ff0797cd852bf7b84d0673866c74ac
Javohir <xasanof17@gmail.com>
2022-06-23 20:58:56 +0500

    step 1

---
+++ src/components/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <header class="header">
+    <div class="header__top">
+      <p class="header__top-text">Free shipping on orders over $50</p>
+    </div>
+      <nav class="header__nav">
+        <div class="container">
+          <div class="header__nav-content">
+            <ul class="header__nav-content_list">
+              <li class="header__nav-content_list-item"><a href="#!" class="header__nav-content_list-item_link">shop</a></li>
+              <li class="header__nav-content_list-item"><a href="#!" class="header__nav-content_list-item_link">membership</a></li>
+              <li class="header__nav-content_list-item"><a href="#!" class="header__nav-content_list-item_link">our story</a></li>
+            </ul>
+            <a href="#!" class="logo">logo</a>
+              <ul class="header__nav-content_list">
+              <li class="header__nav-content_list-item"><a href="#!" class="header__nav-content_list-item_link">Newsletter</a></li>
+              <li class="header__nav-content_list-item"><a href="#!" class="header__nav-content_list-item_link">Account</a></li>
+              <li class="header__nav-content_list-item"><a href="#!" class="header__nav-content_list-item_link">Cart (<span>0</span>)</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<!-- CSS libraries -->
+<style src="normalize.css/normalize.css">
+</style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  list-style: none;
+}
+<style>
+</style>
+
+<!-- Scoped component css -->
+<!-- It only affect current component -->
+<style scoped>
+
+</style>
